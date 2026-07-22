f97c56dc1a1192f324878ad552396ab9b302f9fd
Gerson Pereira <gersonpereira@MacBook-Air-de-Gerson.local>
2026-07-21 22:30:12 -0300

    Realizamos a correção na sintaxe e lógica do código

--- cadastro_equipe.py
+++ cadastro_equipe.py
@@ -1,17 +1,14 @@
-nome_equipe = "";
-nome_lider = "";
-ano_fundacao = "";
-print("Digite o nome da equipe: ");
-input(nome_equipe);
-print("----------");
-print("Digite o nome do líder da equipe: ");
-input(nome_lider);
-print("----------");
-print("Digite o ano de fundação da equipe: ");
-input(ano_fundacao);
-print("----------");
-print("Sua o nome da sua equipe é: ", nome_equipe);
-print("O nome do líder da sua equipe é: ", nome_lider);
-print("O ano de fundação da sua equipe é: ", ano_fundacao);    
-print("----fim----");
-print("Cadastro realizado com sucesso!");
+# Este programa solicita ao usuário informações sobre uma equipe e exibe essas informações na tela.
+nome_equipe = input("Digite o nome da equipe: ")
+nome_lider = input("Digite o nome do líder da equipe: ")
+ano_fundacao = input("Digite o ano de fundação da equipe: ")
+
+print("\nInformações da equipe cadastrada:")
+
+# Exibe as informações fornecidas pelo usuário
+print(f"O nome da sua equipe é: {nome_equipe}")
+print(f"Nome do líder de sua equipe é: {nome_lider}")
+print(f"Ano de fundação da sua equipe é: {ano_fundacao}")
+print("---fim---")
+
+print("Cadastro realizado com sucesso!")
