184f97b42ca7d979227671850dd815905b9ffcaf
Gerson Pereira <gersonpereira@MacBook-Air-de-Gerson.local>
2026-07-25 14:18:46 -0300

    Refactor: ajustando o código e cirando funções

--- cadastro_equipe.py
+++ cadastro_equipe.py
@@ -1,3 +1,18 @@
+# Definindo funções do programa
+def calcular_pontuacao(qtde_membros):
+    pontos = qtde_membros * 50
+    return pontos
+
+def verificar_qualificacao(qtde_membros): 
+    if qtde_membros >= 10:
+        return print("STATUS: Equipe Qualificada!!!!")
+    else:
+        return print("STATUS: Equipe NÃO Qualificada!!!!")
+
+def calcular_idade(ano_fundacao):
+    idade = 2026 - int(ano_fundacao)
+    return idade
+
 lista_equipes = []
 while True:
      # Este programa solicita ao usuário informações sobre uma equipe e exibe essas informações na tela.
@@ -13,27 +28,22 @@ while True:
     print(f"-- O nome da sua equipe é: {nome_equipe}")
     print(f"-- Nome do líder de sua equipe é: {nome_lider}")
     print(f"-- Ano de fundação da sua equipe é: {ano_fundacao}")
-    print(f"-- Sua equipe tem {int(2026) - int(ano_fundacao)} anos de existência")
+    print(f"-- Sua equipe tem {calcular_idade(ano_fundacao)} anos de existência")
+    # print(f"-- Sua equipe tem {int(2026) - int(ano_fundacao)} anos de existência")
     print(f"-- A quantidade de membros da sua equipe é: {qtde_membros}")
-    pontuacao_inicial = qtde_membros * 50
+    pontuacao_inicial = calcular_pontuacao(qtde_membros)
     print(f"-------: A pontuação inicial da sua equipe é: {pontuacao_inicial}")
+    verificar_qualificacao(qtde_membros)
     print(" ")
     # print("Cadastro realizado com sucesso!")
     print(" ")
     dados_equipe = {"nome": nome_equipe, 
                     "lider": nome_lider,
-                    "ano": ano_fundacao, 
+                    "ano": ano_fundacao,
+                    "idade": calcular_idade(ano_fundacao), 
                     "membros": qtde_membros, 
                     "pontuacao": pontuacao_inicial}
     lista_equipes.append(dados_equipe)
-    if qtde_membros >= 10:
-        print("STATUS: Equipe Qualificada!!!!")
-        print(" ")
-        print(" ")
-    else:
-        print("STATUS: Equipe NÃO Qualificada!!!!")
-        print(" ")
-        print(" ")
     continuar = input("Deseja cadastrar outra equipe? (s/n): ")
     if continuar.upper() == 'N':
         break
@@ -41,6 +51,7 @@ print("--- FIM DO PROCESSO DE CREDENCIAMENTO ---")
 print("### Programa finalizado!")
 print(" ")
 print(" ")
+print("Listagem das equipes cadastradas:")
 for equipe in lista_equipes:
-    print(f"-- Equipe: {equipe['nome']} \n| Líder: {equipe['lider']} \n| Ano de fundação: {equipe['ano']} \n| Quantidade de membros: {equipe['membros']} \n| Pontuação inicial: {equipe['pontuacao']}")
+    print(f"\n-- Equipe: {equipe['nome']} \n| Líder: {equipe['lider']} \n| Ano de fundação: {equipe['ano']}\n| Idade: {equipe['idade']} \n| Quantidade de membros: {equipe['membros']} \n| Pontuação inicial: {equipe['pontuacao']}\n")
 #print(f"Equipes cadastradas: {', '.join(lista_equipes)}")
---
+++ hello_gincana.py
@@ -0,0 +1,7 @@
+print("Ambiente configurado com sucesso! Seja bem-vindo ao Sistema de  Gincana!")
+#
+nome_equipe = 'FALCÕES';
+pontos_iniciais = 0;
+#
+print("Nome da equipe: ", nome_equipe);
+print("A Equipe ", nome_equipe, " tem ", pontos_iniciais, " pontos iniciais.");
