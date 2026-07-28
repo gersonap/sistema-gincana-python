e4cfc14465ca62959f2f18bdb28d4cb02ee75f2a
Gerson Pereira <gersonpereira@MacBook-Air-de-Gerson.local>
2026-07-27 21:22:26 -0300

    Feat: adicionando método de pontuação e penalidades na classe

--- cadastro_equipe.py
+++ cadastro_equipe.py
@@ -1,11 +1,18 @@
 class Equipe:
-    def __init__(self, nome, lider, ano_fundacao, qtde_membros):
+    def __init__(self, nome, lider, ano_fundacao, qtde_membros, pontuacao):
         self.nome = nome
         self.lider = lider
         self.ano_fundacao = int(ano_fundacao)
         self.qtde_membros = int(qtde_membros)
         self.idade = 2026 - self.ano_fundacao
-        self.pontuacao_inicial = self.qtde_membros * 50
+        # self.pontuacao_inicial = self.qtde_membros * 50
+        self.pontuacao = pontuacao
+
+    def registrar_pontuacao(self, valor):
+        self.pontuacao += valor
+
+    def aplicar_penalidade(self, valor):
+        self.pontuacao -= valor
 
     def verificar_qualificacao(self):
         if self.qtde_membros >= 5:
@@ -22,7 +29,8 @@ while True:
     nome_lider = input("Digite o nome do líder da equipe: ")
     ano_fundacao = input("Digite o ano de fundação da equipe: ")
     qtde_membros = int(input("Digite a quantidade de membros da equipe: "))
-    nova_equipe = Equipe(nome_equipe, nome_lider, ano_fundacao, qtde_membros)
+    pontuacao = int(input("Digite a pontuação inicial da equipe: "))
+    nova_equipe = Equipe(nome_equipe, nome_lider, ano_fundacao, qtde_membros, pontuacao)
     lista_equipes.append(nova_equipe)
     print("\nInformações da equipe cadastrada:")
 
@@ -32,7 +40,7 @@ while True:
     print(f"-- Ano de fundação da sua equipe é: {ano_fundacao}")
     print(f"-- Sua equipe tem {nova_equipe.idade} anos de existência")
     print(f"-- A quantidade de membros da sua equipe é: {qtde_membros}")
-    print(f"-------: A pontuação inicial da sua equipe é: {nova_equipe.pontuacao_inicial}")
+    print(f"-------: A pontuação inicial da sua equipe é: {nova_equipe.pontuacao} pontos")
     print()
     continuar = input("Deseja cadastrar outra equipe? (s/n): ")
     if continuar.upper() == 'N':
@@ -44,4 +52,10 @@ print()
 print("Listagem das equipes cadastradas:")
 
 for equipe in lista_equipes:
-    print(f"\n-- Equipe: {equipe.nome} \n| Líder: {equipe.lider} \n| Ano de fundação: {equipe.ano_fundacao}\n| Idade: {equipe.idade} \n| Quantidade de membros: {equipe.qtde_membros} \n| Pontuação inicial: {equipe.pontuacao_inicial}\n| {equipe.verificar_qualificacao()}\n")
+    print(f"\n-- Equipe: {equipe.nome} \n| Líder: {equipe.lider} \n| Ano de fundação: {equipe.ano_fundacao}\n| Idade: {equipe.idade} \n| Quantidade de membros: {equipe.qtde_membros} \n| Pontuação inicial: {equipe.pontuacao}\n| {equipe.verificar_qualificacao()}\n")
+
+# Realizando testes de pontuação e penalidade
+print("### Testando pontuação e penalidade ###")
+lista_equipes[0].registrar_pontuacao(100)
+print(f"A equipe {lista_equipes[0].nome} agora tem pontuação {lista_equipes[0].pontuacao} pontos!")
+
