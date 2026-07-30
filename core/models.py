82772f8644436d111a5e74203932eb38babca666
Gerson Pereira <gersonap@gmail.com>
2026-07-30 18:07:50 -0300

    FEAT: configura o app core e cria o model com o migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Equipe(models.Model):
+    nome = models.CharField(max_length=100)
+    lider = models.CharField(max_length=100)
+    ano_fundacao = models.IntegerField()
+    qtde_membros = models.IntegerField()
+    pontuacao = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.nome
